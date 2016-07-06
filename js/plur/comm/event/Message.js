096fae18164639784539ee68a5f9fa37ea193ebe
roylaurie <roy.laurie@asimovian.software>
2016-07-06 10:40:01 -0700

    wip crypto events

--- js/plur/comm/event/Message.js
+++ js/plur/comm/event/Message.js
@@ -7,7 +7,7 @@
 
 define([
     'plur/PlurObject',
-    'plur/event/Event',
+    'plur/event/AEvent',
     'plur/hash/Singleton'],
 function(
     PlurObject,
@@ -25,76 +25,19 @@ function(
  * @param {string} encryptedMessage This will be transmitted instead of the unencrypted message contents.
  * @param {string} encryptedNextKey Provided for use with Symmetric ciphers.
  */
-var MessageEvent = function(message, encryptedMessage, encryptedNextKey) {
-    Event.call(this);
+var MessageEvent = function(recipientPublicKeyHash, senderPublicKeyHash, message) {
+    AEvent.call(this);
 
-    if (!PlurObject.implementing(message, IMessage)) {
+    if (message !== null && !PlurObject.implementing(message, IMessage)) {
         throw new TypeError('Invalid message', {message: message});
-    } else if (typeof encryptedMessage !== 'undefined' && typeof encryptedMessage !== 'string') {
-        throw new TypeError('Encrypted message is not a string', {encryptedMessage: encryptedMessage});
     }
 
-    this._timestamp = message.getTimestamp(); // override
-    this._recipientPublicKeyHash = message.getRecipientPublicKeyHash();
-    this._senderPublicKeyHash = message.getSenderPublicKeyHash();
-    this._channelEventType = null;
-    this._channelResponseEventType = null;
-
-    if (typeof encryptedMessage !== 'undefined') {
-        this._encrypted = true;
-        this._message = encryptedMessage;
-        this._encryptedNextKey = encryptedNextKey;
-    } else {
-        this._encrypted = false;
-        this._message = message;
-        this._encryptedNextKey = null;
-    }
-
-    this._hash = Hash.get().hash(this._message);
+    this._recipientPublicKeyHash = recipientPublicKeyHash;
+    this._senderPublicKeyHash = senderPublicKeyHash;
+    this._message = message;
 };
 
-MessageEvent.prototype = PlurObject.create('plur/comm/MessageEvent', MessageEvent);
-PlurObject.implement(MessageEvent, Hash.IHashable);
-
-
- //* @param {Function():=string encrypted|undefined} encryptFunction Returns an encrypted copy of IMessage, no parameters passed.
-MessageEvent.createEncrypted = function(message, encryptFunction, modelTransformer, encryptNextKeyFunction) {
-    var dataEncryptionPromise = null;
-    var keyEncryptionPromise = null;
-    var encryptedModel = null;
-    var encryptedNextKey;
-
-    if (typeof encryptNextKeyFunction === 'function') {
-        keyEncryptionPromise = new PlurPromise(function(resolve, reject) {
-            encryptNextKeyFunction().then(function(encryptedData) {
-                encryptedNextKey = encryptedData;
-                resolve();
-            });
-        });
-    } else {
-        keyEncryptionPromise = new PlurPromise(PlurPromise.noop);
-    }
-
-    dataEncryptionPromise = new PlurPromise(function(resolve, reject) {
-        encryptFunction(modelTransformer).then(function(encryptedData) {
-            encryptedModel = encryptedData;
-            resolve());
-        });
-    });
-
-    var promise = new PlurPromise(function(resolve, reject) {
-        PlurPromise.all([keyEncryptionPromise, dataEncryptionPromise]).then(function() {
-            resolve(new MessageEvent(message, encryptedModel, encryptedNextKey));
-        });
-    });
-
-    return promise;
-};
-
-MessageEvent.prototype.hash = function() {
-    return this._hash;
-};
-
+MessageEvent.prototype = PlurObject.create('plur/comm/MessageEvent', MessageEvent, AEvent);
 
 return Event;
 });
